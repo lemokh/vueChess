e1da0ddd19e3c4539b920c32ed3050bd46db4859
byndasma <byndasma@gmail.com>
2018-05-23 21:56:51 -0700

    app.vue

---
+++ vue/app.vue
@@ -0,0 +1,15 @@
+<script>
+
+new Vue({
+    el: '#app',
+    data: {
+        clicks: 0,
+    },
+    methods: {
+        handleCount(clicks) {
+            this.clicks = clicks;
+        }
+    }
+});
+
+</script>
